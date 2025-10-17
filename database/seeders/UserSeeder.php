a90bb0a3d2d431bdf052295911c2a13f4099dbc9
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-10-17 11:39:09 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
