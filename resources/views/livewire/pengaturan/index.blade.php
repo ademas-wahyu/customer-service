28376f5aae9a9a21b1a1850a9ed98ec098a9cf0e
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-10-21 09:54:49 +0700

    tambah menu di pengaturan

--- resources/views/livewire/pengaturan/index.blade.php
+++ resources/views/livewire/pengaturan/index.blade.php
@@ -1,11 +1,59 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                <h3 class="text-lg font-medium">Halaman Pengaturan</h3>
-                <p class="mt-2 text-sm text-gray-600">
-                    Demo Pengaturan untuk Customer Service.
-                </p>
+        <div class="grid grid-cols-2 gap-4">
+            <div class="p-4 flex flex-col justify-start items-start gap-4">
+                <div class="p-5 bg-white rounded-lg shadow-md w-full">
+                    <h3 class="text-gray-800 font-bold text-2xl">Akun</h3>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Ubah Profil</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Ubah</a>
+                    </div>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Ubah Kata Sandi</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Ubah</a>
+                    </div>
+                </div>
+
+                <div class="p-5 bg-white rounded-lg shadow-md w-full">
+                    <h3 class="text-gray-800 font-bold text-2xl">Manajemen Data</h3>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Ekspor Data</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Kelola</a>
+                    </div>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Impor Data</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Impor</a>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4 flex flex-col justify-between items-center gap-4">
+                <div class="p-5 bg-white rounded-lg shadow-md w-full">
+                    <h3 class="text-gray-800 font-bold text-2xl">Bantuan</h3>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Pusat Bantuan</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Hubungi</a>
+                    </div>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Hubungi Kami</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Kontak</a>
+                    </div>
+                </div>
+
+                <div class="p-5 bg-white rounded-lg shadow-md w-full">
+                    <h3 class="text-gray-800 font-bold text-2xl">Tentang Aplikasi</h3>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Versi Aplikasi</span>
+                        <span class="text-gray-500 font-regular hover:text-gray-600">Alpha</sp>
+                    </div>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Kebijakan Privasi</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Lihat</a>
+                    </div>
+                    <div class="flex justify-between items-center mt-4">
+                        <span>Syarat & Ketentuan</span>
+                        <a href="#" class="text-navy-800 font-medium hover:text-navy-900">Lihat</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
