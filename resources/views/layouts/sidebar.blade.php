f04377bf40d8db15f5bd33e5c260c76720d53075
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-10-21 13:40:41 +0700

    add Icoons

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -24,9 +24,7 @@
             Akun CS
         </a>
         <a href="{{ route('rekapan.index') }}" class="flex items-center px-4 py-2 rounded-md hover:bg-navy-700 hover:text-white {{ request()->routeIs('rekapan.index') ? 'bg-navy-700 text-white' : '' }}">
-            <svg class="w-5 h-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75c0-.231-.035-.454-.1-.664M6.75 7.5H18v2.25H6.75V7.5Z" />
-            </svg>
+            <x-icons.clipboard-list />
             Rekapan
         </a>
 
