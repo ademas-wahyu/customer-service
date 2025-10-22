cf8ee4a37b1156a60e866ab644d1b9ba250c2e83
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-10-22 15:36:17 +0700

    Fix migration rollback for profile photo

--- database/migrations/2025_10_18_132704_add_profile_photo_path_to_users_table.php
+++ database/migrations/2025_10_18_132704_add_profile_photo_path_to_users_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_photo_path')->nullable()->after('email');
+        Schema::table("users", function (Blueprint $table) {
+            $table->string("profile_photo_path")->nullable()->after("email");
         });
     }
 
@@ -21,8 +20,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table("users", function (Blueprint $table) {
+            $table->dropColumn("profile_photo_path");
         });
     }
 };
