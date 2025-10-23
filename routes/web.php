c67188a6c798dca3c72849d97867b6f76e892c2f
ademaswahyu <53292337+ademas-wahyu@users.noreply.github.com>
2025-10-23 08:37:04 +0700

    Remove duplicate profile routes

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class)->middleware(['auth'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('/akun-cs', AkunCsIndex::class)->middleware(['auth'])->name('akun-cs.index');
 
 Route::get('/pengaturan', PengaturanIndex::class)->middleware(['auth'])->name('pengaturan.index');
